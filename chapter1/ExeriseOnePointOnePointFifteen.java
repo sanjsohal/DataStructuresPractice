528e48b5b53296e572fc200a1d2f544c6bf379e9
Sajinder Singh <sanj.sohal@gmail.com>
2026-07-11 16:21:00 +0530

    Histogram problem to solve

---
+++ chapter1/ExeriseOnePointOnePointFifteen.java
@@ -0,0 +1,21 @@
+package chapter1;
+import edu.princeton.cs.algs4.*;
+public class ExerciseOnePointOnePointFifteen {
+    public static void main(String...args) {
+        System.out.println("Enter the upper bound value of each array in the input");
+        int M = StdIn.readInt();
+        System.out.println("Enter length of input array:");
+        int lengthOfInputArray = StdIn.readInt();
+        int[] inputArray = new int[lengthOfInputArray];
+        int i = 0; 
+        while(i<lengthOfInputArray) {
+            inputArray[i] = StdIn.readInt();
+            if(inputArray[i]<0 || inputArray[i] > (M-1)) throw new IllegalArgumentException("Each element should be between 0 and M-1");
+            i++;
+        }
+    }
+    static int[] histogram(int[] a, int M) {
+        
+        return null;
+    }
+}
