900cb184ac33e45eeef6351fab4986403afa38a4
Sajinder Singh <sanj.sohal@gmail.com>
2026-07-12 15:26:09 +0530

    Improved Fibonacci recursion code

--- chapter1/Fibonacci.java
+++ chapter1/Fibonacci.java
@@ -1,9 +1,21 @@
 package chapter1;
 import edu.princeton.cs.algs4.*;
+import java.math.BigInteger;
 public class Fibonacci {
-    public static long F(int N) {
-        if(N == 0 || N == 1) return N;
-        return F(N-1)+F(N-2);
+    static BigInteger[] computedValues = new BigInteger[100];
+    public static BigInteger F(int N) {
+        if(N == 0) {
+            computedValues[0] = new BigInteger("0");
+            return new BigInteger("0");
+        }
+        if(N == 1) {
+            computedValues[1] = new BigInteger("1");
+            return new BigInteger("1");
+        }
+        if(computedValues[N] != null) return computedValues[N];
+        BigInteger value = F(N-1).add(F(N-2));
+        computedValues[N] = value;
+        return value;
     }
     public static void main(String...args) {
         for(int N = 0; N < 100; N++) {
